785e3819aab2ef8a4f3e3d38aae28ecc6f56bd24
Aditya Shejwal <adityashejwal016@gmail.com>
2026-01-17 17:41:07 +0530

    Add files via upload

---
+++ Assginment_28/rowcol1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+   int i = 0;
+   int j = 0;
+   char ch = 'A';
+
+   for(i = 1; i<= iRow; i++)
+   {
+    ch = 'A';
+      for(j = 1; j<= iCol; j++,ch++)
+      {
+         printf("%c\t",ch);
+         
+      }
+      printf("\n");
+   }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows :\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns :\n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
+

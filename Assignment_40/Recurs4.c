474df51fee020f86afd7917c2d2f7eff02315de6
Aditya Shejwal <adityashejwal016@gmail.com>
2026-01-28 17:52:57 +0530

    Add files via upload

---
+++ Assignment_40/Recurs4.c
@@ -0,0 +1,28 @@
+// input : 5
+// output: A B C D E
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    char ch = 'A';
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%c\t", ch);
+        ch++;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the Frequency : ");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+    return 0;
+}
